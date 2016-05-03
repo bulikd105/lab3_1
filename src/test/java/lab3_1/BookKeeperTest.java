3297d91da37bdedf2bf398e1b0b521969c8a8572
bulikd105 <bulikd105@gmail.com>
2016-05-03 18:07:35 +0200

    Pierwszy test. Jedna faktura - jedna pozycja

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -1,17 +1,24 @@
 package lab3_1;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Date;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Tax;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest 
@@ -31,6 +38,24 @@ public class BookKeeperTest
 	@Test
 	public final void test_setOne_getOne() 
 	{
+		clientData 		= new ClientData(Id.generate(), "nieistotne");
+		invoice 		= new Invoice(Id.generate(), clientData);
+		invoiceRequest 	= new InvoiceRequest(clientData);
+		price 			= new Money(10);
+		productData 	= new ProductData(Id.generate(), price, "nieistotne", ProductType.DRUG, new Date());
+		item 			= new RequestItem(productData, 0, new Money(10));
+		
+		invoiceRequest.add(item);
 
+		invoiceFactory 	= Mockito.mock(InvoiceFactory.class);
+		taxPolicy 		= Mockito.mock(TaxPolicy.class);
+		
+		Mockito.when(invoiceFactory.create(invoiceRequest.getClientData())).thenReturn(invoice);
+		Mockito.when(taxPolicy.calculateTax(ProductType.DRUG, price)).thenReturn(new Tax(new Money(1d), "nieistotne"));
+		
+		bookKeeper 		= new BookKeeper(invoiceFactory);
+		newInvoice 		= bookKeeper.issuance(invoiceRequest, taxPolicy);
+		
+		assertThat(newInvoice.getItems().size(), is(1));
 	}
 }
