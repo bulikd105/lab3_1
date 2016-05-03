c729e438a8537d0899dbb3dc846d373511d64f20
bulikd105 <bulikd105@gmail.com>
2016-05-03 18:14:28 +0200

    Utworzenie nowej klasy. AddProductCommandHandlerTest

---
+++ src/test/java/lab3_1/AddProductCommandHandlerTest.java
@@ -0,0 +1,14 @@
+package lab3_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddProductCommandHandlerTest 
+{
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
