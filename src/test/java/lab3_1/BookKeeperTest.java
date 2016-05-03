2373acc623f78801e891b0508687eb2d52c7c0f7
bulikd105 <bulikd105@gmail.com>
2016-05-03 17:34:56 +0200

    Dodanie klasy BookKeeperTest

---
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -0,0 +1,18 @@
+package lab3_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+
+public class BookKeeperTest 
+{
+	BookKeeper bookKeeper;
+	
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
