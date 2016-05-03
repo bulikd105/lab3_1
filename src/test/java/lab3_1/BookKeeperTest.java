5a1e7a0da434624c488656dd8f62280fab0cdf51
bulikd105 <bulikd105@gmail.com>
2016-05-03 18:04:23 +0200

    Niezbedbne deklaracje w klasie do testow

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -4,15 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest 
 {
 	BookKeeper bookKeeper;
 	
+	ClientData 		clientData;
+	Invoice 		invoice;
+	Invoice 		newInvoice;
+	InvoiceRequest 	invoiceRequest;
+	Money 			price;
+	ProductData 	productData;
+	RequestItem 	item;
+	InvoiceFactory 	invoiceFactory;
+	TaxPolicy 		taxPolicy;
+	
 	@Test
-	public void test() 
+	public final void test_setOne_getOne() 
 	{
-		fail("Not yet implemented");
+
 	}
 }
