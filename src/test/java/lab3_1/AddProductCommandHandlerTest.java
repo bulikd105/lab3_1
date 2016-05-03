bbae81703e58d1891407c8c622c9dc63d72c77d9
bulikd105 <bulikd105@gmail.com>
2016-05-03 18:29:16 +0200

    Dodanie kilku deklaracji

--- src/test/java/lab3_1/AddProductCommandHandlerTest.java
+++ src/test/java/lab3_1/AddProductCommandHandlerTest.java
@@ -3,9 +3,45 @@ package lab3_1;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandHandler;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 public class AddProductCommandHandlerTest 
 {
+	@Mock
+	ReservationRepository 		reservationRepository;
+	
+	@Mock
+	ProductRepository 			productRepository;
+	
+	@Mock
+	SuggestionService 			suggestionService;
+	
+	@Mock
+	ClientRepository 			clientRepository;
+	
+	@Mock
+	SystemContext 				systemContext;
+
+	@InjectMocks
+	AddProductCommandHandler 	addProductCommandHandler = new AddProductCommandHandler();
+	
+	Id 							orderID;
+	Product 					product;
+	Reservation 				reservation;
+	AddProductCommand 			addProductCommand;
+
 	@Test
 	public void test() 
 	{
