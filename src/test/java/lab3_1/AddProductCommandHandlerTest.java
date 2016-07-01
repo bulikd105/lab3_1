56aa8ce48cff03e6d87a5bb654250cf439d8ff2d
bulikd105 <bulikd105@gmail.com>
2016-07-01 19:04:47 +0200

    Zmiany w klasie AddProductCommandHandlerTest.

--- src/test/java/lab3_1/AddProductCommandHandlerTest.java
+++ src/test/java/lab3_1/AddProductCommandHandlerTest.java
@@ -1,16 +1,20 @@
 package lab3_1;
 
-import static org.junit.Assert.*;
-
-import java.util.Date;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import builder.AddProductCommandBuilder;
+import builder.ProductBuilder;
+import builder.ReservationBuilder;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandHandler;
@@ -18,65 +22,60 @@ import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
-import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
-import pl.com.bottega.ecommerce.sharedkernel.Money;
-import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation.ReservationStatus;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AddProductCommandHandlerTest 
 {
-	@Mock
-	ReservationRepository 		reservationRepository;
-	
-	@Mock
-	ProductRepository 			productRepository;
-	
-	@Mock
-	SuggestionService 			suggestionService;
-	
-	@Mock
-	ClientRepository 			clientRepository;
-	
-	@Mock
-	SystemContext 				systemContext;
 
 	@InjectMocks
-	AddProductCommandHandler 	addProductCommandHandler = new AddProductCommandHandler();
+	AddProductCommandHandler addProductCommandHandler;
 	
-	Id 							orderID;
-	Id							productID;
-	int 						quantity;
-	ReservationStatus 			reservationStatus;
-	ClientData 					clientData; 
-	Date 						createDate;
+	@Mock
+	ReservationRepository reservationRepository;
+	@Mock
+	ProductRepository productRepository;
+	@Mock
+	SuggestionService suggestionService;
+	@Mock
+	ClientRepository clientRepository;
+	@Mock
+	SystemContext systemContext;
 	
-	Product 					product;
-	Reservation 				reservation;
-	AddProductCommand 			addProductCommand;
-
 	@Test
-	public final void test_hasReservationBeedSaved() 
+	public final void testHandle_testState_reservationHasBeenSaved() 
 	{
-		orderID 			= Id.generate();
-		productID			= Id.generate();
-		quantity 			= 1;
-		reservationStatus	= ReservationStatus.OPENED;
-		clientData 			= new ClientData(Id.generate(), "nieistotne");
-		createDate			= new Date();
-		
-		product 			= new Product(Id.generate(),new Money(10d),"nieistotne",ProductType.STANDARD);
-		reservation 		= new Reservation(Id.generate(),reservationStatus,clientData,createDate);
-		addProductCommand 	= new AddProductCommand(orderID, productID, quantity);
+		// when
+		Product product = new ProductBuilder().build();
+		Reservation reservation = new ReservationBuilder().build();
+		AddProductCommand addProductCommand = new AddProductCommandBuilder().build();
 
 		MockitoAnnotations.initMocks(addProductCommandHandler);
-		
 		Mockito.when(reservationRepository.load(Mockito.any(Id.class))).thenReturn(reservation);
 		Mockito.when(productRepository.load(Mockito.any(Id.class))).thenReturn(product);
-		
 		addProductCommandHandler.handle(addProductCommand);
 
+		// then
 		Mockito.verify(reservationRepository, Mockito.times(1)).save(reservation);
+
+	}
+
+	@Test
+	public final void testHandle_testState_reservationRipositoryLoadproperOrder() 
+	{
+		Id orderID = new Id("167");
+		Product product = new ProductBuilder().build();
+		Reservation reservation = new ReservationBuilder().build();
+		AddProductCommand addProductCommand = new AddProductCommandBuilder().withOrderId(orderID).build();
+
+		MockitoAnnotations.initMocks(addProductCommandHandler);
+		Mockito.when(reservationRepository.load(Mockito.any(Id.class))).thenReturn(reservation);
+		Mockito.when(productRepository.load(Mockito.any(Id.class))).thenReturn(product);
+		addProductCommandHandler.handle(addProductCommand);
+
+		// then
+		Mockito.verify(reservationRepository, Mockito.times(1)).load(orderID);
 	}
 }
